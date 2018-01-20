44fdf1b8c6017a1fc93c73fd8a73f00e7fc7eea3
T-530\ukolaj <ukolaj.skoski@gmail.com>
2018-01-20 15:58:28 +0100

    teleporting, camera texture

---
+++ Assets/scripts/random_country.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int interval = 3;
+    private DateTime lastTime;
+    public List<string> countries;
+    private System.Random random;
+    public TextMesh textMesh;
+
+	// Use this for initialization
+	void Start()
+    {
+        lastTime = DateTime.Now;
+        random = new System.Random();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        string country;
+		if (DateTime.Now > lastTime.AddSeconds(interval))
+        {
+            lastTime = DateTime.Now;
+            country = countries[random.Next(countries.Count)];
+            textMesh.text = country;
+        }
+	}
+}
---
+++ Assets/scripts/teleporting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporting : MonoBehaviour
+{
+	public GameObject destination;
+    public string whatTeleport = "Player";
+
+	void OnTriggerEnter(Collider collider)
+	{
+        if (collider.CompareTag(whatTeleport) || collider.gameObject.layer == 1)
+		{
+			Vector3 targetPosition = destination.transform.position;
+			targetPosition += destination.transform.forward * 2; //new Vector3 (1, 1, 5);
+			collider.transform.position = targetPosition;
+                
+            if (destination.GetComponent<AudioSource>())
+			destination.GetComponent<AudioSource>().Play();
+		}
+	}
+}
